40945567ccbc5a7c4943ad33103d9ffd738096ea
Frank <franklinewax@gmail.com>
2020-06-20 13:16:38 +0300

    Installing bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+// 1414caf962b667337d521961b7ab11a48194957e 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
