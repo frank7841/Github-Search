5cd8b79a79135481b4b9ef7db9e05ef0a5c6cc52
Frank <franklinewax@gmail.com>
2020-06-21 22:55:01 +0300

    Created apiKey objevt that stores github Api Key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'1414caf962b667337d521961b7ab11a48194957e' 
 };
 
 /*
