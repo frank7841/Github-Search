3c345f89ed23394d27735e5383aedb93f2a1fbbc
Frank <franklinewax@gmail.com>
2020-06-21 23:40:00 +0300

    Created function userSearch to return git hub userdetails

--- src/app/services/myservice.service.ts
+++ src/app/services/myservice.service.ts
@@ -13,4 +13,31 @@ export class MyserviceService {
   constructor(private http: HttpClient) {
     this.userFind = new Users("","","","",0,0,0,"",new Date);
    }
+   userSeach(nameSearch: string){
+     interface Responce {
+      url:string,
+      login: string;
+      html_url:string;
+      location:string
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+
+     }
+     return new Promise((resolve, reject) => {
+      this.http.get<Responce>('https://api.github.com/users/'+nameSearch+'?access_token='+environment.apiKey).toPromise().then(
+        (result) => {
+          this.userFind = result;
+          console.log(this.userFind);
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+  }
 }
