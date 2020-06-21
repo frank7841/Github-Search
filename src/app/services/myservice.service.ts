2a5226bb45649356bf951bf442cde4692c97d1c8
Frank <franklinewax@gmail.com>
2020-06-21 23:25:26 +0300

    importing the necessary objects to myservices

--- src/app/services/myservice.service.ts
+++ src/app/services/myservice.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import{Users} from '../users';
+import{HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
