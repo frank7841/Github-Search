b3840056ddb16919715c5f90f423ed93c9488ebb
Frank <franklinewax@gmail.com>
2020-06-22 06:15:28 +0300

    Importing myservices and class Users

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MyserviceService} from '../services/myservice.service';
+import {Users} from '../users'
+
 
 @Component({
   selector: 'app-header',
