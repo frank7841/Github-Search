0203b189f61e05b475a88e282123bec7d4ea329f
Frank <franklinewax@gmail.com>
2020-06-22 10:55:59 +0300

    creating get Repo function on header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MyserviceService} from '../services/myservice.service';
-import {Users} from '../users'
+import {Users} from '../users';
+import {Repos} from '../repos';
 
 
 @Component({
@@ -10,8 +11,9 @@ import {Users} from '../users'
 })
 export class HeaderComponent implements OnInit {
   user:Users;
+  repo:Repos;
 
-  constructor(public getService: MyserviceService) { }
+  constructor(public getService: MyserviceService, private repoService:MyserviceService) { }
   searchs(nameSearch){
     this.getService.userSeach(nameSearch).then(
       (success)=>{
@@ -20,8 +22,17 @@ export class HeaderComponent implements OnInit {
       (error)=>{
         console.log(error)
       }
-    )
-  }
+    );
+    this.repoService.getRepo(nameSearch).then(
+      (results)=>{
+        this.repo =this.repoService.allRepos
+        console.log(this.repo);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+}
 
   ngOnInit(){
     this.searchs('frank-simiyu')
