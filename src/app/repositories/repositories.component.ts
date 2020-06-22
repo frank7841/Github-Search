575e136d2c7c05cba081a129c43da50c24a0c3db
Frank <franklinewax@gmail.com>
2020-06-22 10:37:35 +0300

    define class repo and creating getRipo function

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MyserviceService} from '../services/myservice.service'
 
 @Component({
   selector: 'app-repositories',
--- src/app/services/myservice.service.ts
+++ src/app/services/myservice.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import{Users} from '../users';
 import{HttpClient} from '@angular/common/http';
 import {environment} from 'src/environments/environment.prod';
+import {Repos} from '../repos'
 
 
 @Injectable({
